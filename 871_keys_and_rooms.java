bad8158e6378e4ac9e522192407b7d35a6e30df9
Madhu_Sri <123156637+Madhusri02@users.noreply.github.com>
2025-02-18 11:53:02 +0530

    Update 871_keys_and_rooms.java - LeetCode Problem 871

--- 871_keys_and_rooms.java
+++ 871_keys_and_rooms.java
@@ -1,40 +1,29 @@
 class Solution {
     public boolean canVisitAllRooms(List<List<Integer>> rooms) {
-        // ArrayList<Integer>[] grp = new ArrayList[rooms.size()];
-        // for(int i = 0 ; i<rooms.size() ; i++){
-        //     grp[i] = new ArrayList<>();
-        // }
-        boolean check[] = new boolean[rooms.size()];
+        int n = rooms.size();
+        boolean check[] = new boolean[n];
         Queue<Integer> qu = new LinkedList<>();
-        // int min = Integer.MAX_VALUE;
-        // for(int i=0 ; i<rooms.size() ; i++){
-        //     for(int j = 0 ; j<rooms.get(i).size() ; j++){
-        //         grp[i].add(rooms.get(i).get(j)); 
-               
-        //         // min = Math.min(min , rooms[i][j]);
-        //     }
-        // }
-
+        int c = 0;
         qu.add(0);
         while(!qu.isEmpty()){
             int rem = qu.poll();
-            
             if(check[rem]!=true){
                 check[rem] = true;
-            for(int num : rooms.get(rem)){
-                System.out.println("rem : " + rem +" the num : " + num);;
-                // check[num] = true;
-                qu.add(num);
-            }
+                c++;
+                for(int num : rooms.get(rem)){
+                    qu.add(num);
+                }
             }
         }
 
-        for(boolean num : check){
-            if(num==false) return false;
+        return c==n;
+
+        // for(boolean num : check){
+        //     if(num==false) return false;
             
-        }
+        // }
 
-        return true;
+        // return true;
         
     }
 }
